868d2b1b3742de1e0dba4a64b052fe404fe27fac
kyle-rader <kyle.rader@ieee.org>
2014-04-04 04:27:44 -0700

    Include stylesheet correctly.

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php 
+$baseInclude = $_SERVER['DOCUMENT_ROOT'] . '/include';
 $page_title = $_SERVER['REQUEST_URI'];
+
 $body = 'app/app.php';
+
 switch($page_title) {
 	case '/':
+	
 		$page_title = 'Icebreaker';
 		break;
 	default:
 		$page_title = 'Not Found';
 }
 
-include_once "include/header.inc";
-include_once "include/db.inc";
+include_once "$baseInclude/header.inc";
+include_once "$baseInclude/resource.inc";
 
 print '<body>';
 include_once "$body";
