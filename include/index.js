03dd7894e1589fd1de712767ca2f5ec08a3a563b
kyle-rader <kyle.rader@ieee.org>
2014-04-06 09:20:19 -0700

    Use cookie to store current page - page load persists refresh now.

--- include/index.js
+++ include/index.js
@@ -1,7 +1,7 @@
-var CURRENT_PAGE = 'home';
 
 $(document).ready(function() {
-	LoadPage(CURRENT_PAGE);
+	var page = getCookie('page') || 'home';
+	LoadPage(page);
 });
 
 function LoadPage(page) {
@@ -21,5 +21,26 @@ function LoadPage(page) {
 
 $(document).on('click', 'a.top-btn', function(event) {
 	var page = $(this).attr('data-target');
+	setCookie('page', page, 1);
 	LoadPage(page);
 });
+
+function getCookie(cname)
+{
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) 
+  {
+  var c = ca[i].trim();
+  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+return "";
+}
+
+function setCookie(cname,cvalue,exdays)
+{
+var d = new Date();
+d.setTime(d.getTime()+(exdays*24*60*60*1000));
+var expires = "expires="+d.toGMTString();
+document.cookie = cname + "=" + cvalue + "; " + expires;
+}
