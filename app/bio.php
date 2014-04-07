4885bf4ab0f3e80e4e346dc1f277ef6cc85aeafa
kyle-rader <kyle.rader@ieee.org>
2014-04-07 14:53:26 -0700

    Add funny links and bio page.

---
+++ app/bio.php
@@ -0,0 +1,17 @@
+<?php 
+// The IceBreaker App
+
+$title = <<<EOT
+<h2>Bio</h2>
+<small>My question to you: Why are you here?</small>
+EOT;
+
+print PageTitle($title);
+?>
+<div class="row">
+	<div class="small-8 columns">
+		<p>
+		Education is and should be treated as a high quality service industry. The students are our customers and if they don't understand something - it is our failure to adapt and evolve the way in which we communicate information.
+		</p>
+	</div>
+</div>
